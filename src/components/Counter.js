32f5a2f5042d0c98f497a2e481af61303e151adc
rdayok <r.dayok@native.semicolon.africa>
2023-10-09 22:26:14 +0100

    used state in a class base component

---
+++ src/components/Counter.js
@@ -0,0 +1,35 @@
+import { Component } from "react";
+
+class Counter extends Component{
+    constructor(){
+        super();
+        this.state = {
+            counter: 0,
+        }
+    }
+
+    increament(){
+        if(this.state.counter !== 30){
+            this.setState({
+                counter: this.state.counter + 1
+            })
+        }
+    }
+    decreament(){
+        if(this.state.counter !== 0){
+            this.setState({
+                counter: this.state.counter - 1
+            })
+        }
+    }
+
+    render(){
+        return <div>
+            <h2>Count value: {this.state.counter} </h2>
+            <button onClick={() => this.increament()}>Increase</button>
+            <button onClick={() => this.decreament()}>Decrease</button>
+        </div> 
+    }
+}
+
+export default Counter;
