3be71902171832fdd48f6e1949c4379a4009c132
rdayok <r.dayok@native.semicolon.africa>
2023-10-09 12:51:03 +0100

    Trying out Class base and Function base components in React

---
+++ src/components/Hello.js
@@ -0,0 +1,10 @@
+// functional components in React
+
+export function Hello2(){
+    return <h1>Hello world</h1>
+}
+
+// Esc6 syntax bellow
+const Hello = ()=> <h1>Hello people</h1>
+
+export default Hello; 
